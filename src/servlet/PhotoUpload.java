66707d34edeeb42c8cf5b60c18d6f3bc3fb50968
liuxiaojun <506391157@qq.com>
2016-04-12 14:34:31 +0800

    用Servlet3.0上传文件

--- src/servlet/PhotoUpload.java
+++ src/servlet/PhotoUpload.java
@@ -1,11 +1,14 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.Collection;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 /**
  * Servlet implementation class PhotoUpload
@@ -21,7 +24,8 @@ public class PhotoUpload extends HttpServlet {
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO 上传一张照片
+		// TODO 上传一张照片,Servlet3.0支持文件上传
+		Collection<Part> photoList = request.getParts();
 	}
 
 }
